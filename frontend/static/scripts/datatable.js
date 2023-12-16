d3375d91f34b3ef9d691a8463da6e363946128c5
egarcia9543 <egarcia9543@soy.sena.edu.co>
2023-12-16 10:15:37 -0500

    Add translation for datatables

--- frontend/static/scripts/datatable.js
+++ frontend/static/scripts/datatable.js
@@ -1,6 +1,9 @@
 /* eslint-disable new-cap */
 $(document).ready(function() {
     const table = $('#datatable').DataTable( {
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json',
+        },
         lengthChange: false,
         buttons: ['copy', 'excel', 'pdf', 'colvis'],
     } );
